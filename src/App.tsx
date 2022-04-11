3d3efc5526e3b83dc32163c706f6c17b26dd4877
Ansaf Nisam <msnfsnpr@gmail.com>
2022-04-11 22:23:23 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const name:string = "ansaf"
+console.log(name)
+
+function App() {
+
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+
+export default App;
